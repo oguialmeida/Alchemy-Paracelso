f3b87147f79615a400f37a12b6a2a93f46064aa4
Guilherme Gomes <gui.rock08@gmail.com>
2023-05-21 16:25:48 -0300

    Update

--- api/main.py
+++ api/main.py
@@ -1,38 +1,70 @@
 from flask import Flask, request
 from sympy import *
 import pandas as pd
+from sklearn.svm import LinearSVC
 
 app = Flask(__name__)
 
-
-def training():
-    # importando os dados de um arquivo externo
-    dados = pd.read_csv("data.csv")
-    dados.head()
+class Data:
+    def __init__(self):
+        self.dados = pd.read_csv("data.csv")
     
-    x = dados[["tipo", "elementos"]]
-    x.head()
+    def get_data(self):
+        return self.dados
+    
+    def add_data(self, tipo, elementos, formula):
+        new_data = pd.DataFrame({'tipo': [tipo], 'elementos': [elementos], 'formula': [formula]})
+        self.dados = pd.concat([self.dados, new_data], ignore_index=True)
+    
+    def update_data(self, index, tipo, elementos, formula):
+        self.dados.loc[index] = [tipo, elementos, formula]
+    
+    def delete_data(self, index):
+        self.dados.drop(index, inplace=True)
 
-    y = dados[["formula"]]
-    y.head()
 
-    # Estanciando o modelo
-    model = LinearSVC()
+class Model:
+    def __init__(self):
+        self.model = LinearSVC()
+    
+    def train_model(self, x, y):
+        self.model.fit(x, y)
+    
+    def predict(self, x):
+        return self.model.predict(x)
 
-    # Treinando o modelo
-    modelo.fit(x,y)
-    previsoes = modelo.predict(x)
-    print("A acurácia do dataset é de: {}%".format(round(accuracy_score(y, previsoes) * 100, 2)))
 
+data_obj = Data()
+model_obj = Model()
+
+@app.route('/data', methods=['GET'])
+def get_data():
+    data = data_obj.get_data()
+    return data.to_json()
+
+@app.route('/data', methods=['POST'])
+def add_data():
+    tipo = request.json['tipo']
+    elementos = request.json['elementos']
+    formula = request.json['formula']
+    data_obj.add_data(tipo, elementos, formula)
+    return {'message': 'Data added successfully'}
+
+@app.route('/data/<int:index>', methods=['PUT'])
+def update_data(index):
+    tipo = request.json['tipo']
+    elementos = request.json['elementos']
+    formula = request.json['formula']
+    data_obj.update_data(index, tipo, elementos, formula)
+    return {'message': 'Data updated successfully'}
+
+@app.route('/data/<int:index>', methods=['DELETE'])
+def delete_data(index):
+    data_obj.delete_data(index)
+    return {'message': 'Data deleted successfully'}
 
 @app.route('/balanceamento', methods=['POST'])
 def balanceamento():
-    pass
-
-
-@app.route('/balanceamento', methods=['POST'])
-def balanceamento():
-
     # Obtenha a equação química enviada pelo cliente
     eq = request.json['equacao']
 
@@ -51,7 +83,15 @@ def balanceamento():
 
     return {'solucao': solucao_str}
 
-# EXEMPLO
-# {
-#     "equacao": "H2 + O2 -> H2O"
-# }
+@app.route('/classify', methods=['POST'])
+def classify():
+    # Obtenha os dados de entrada para classificação
+    tipo = request.json['tipo']
+    elementos = request.json['elementos']
+
+    # Obtenha os dados existentes
+    data = data_obj.get_data()
+
+    # Separe as variáveis independentes (x) e a variável dependente (y)
+    x = data[['tipo', 'elementos']]
+    y = data
